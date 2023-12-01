a8c6bd1594ecffad025b48e679e0daedbaaeadc3
kolba <kolbasa20022002@mail.ru>
2023-12-02 01:55:58 +0300

    add about us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css'],
+  imports: [
+    MatCardModule
+  ],
   standalone: true
 })
 export class AboutUsComponent {

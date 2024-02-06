7eed9e4ad55c06cdb402413bc16f8ba8940c2c65
kolba <kolbasa20022002@mail.ru>
2024-02-06 18:04:59 +0300

    add signIn axios

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule,} from '@angular/forms';
+import {Component} from '@angular/core';
+import {FormsModule, ReactiveFormsModule,} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import axios from "axios";
 
 
 @Component({
@@ -16,17 +17,32 @@ import {ActivatedRoute, Router, RouterLink} from "@angular/router";
   imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatToolbarModule, MatCardModule, MatButtonModule, RouterLink],
 })
 export class SignInComponent  {
-  constructor(private route: Router) {
-  }
+  constructor(private route: Router) {}
+
   public username = '';
   public password = '';
 
 
+  //todo Испрвить метод
   signIn(username: string, password: string) {
-    this.username = username
-    this.password = password
-    alert(username+"   "+password)
-    this.route.navigate(['/'])
+    axios.post('http://localhost:5155/login', {
+      id:1,
+      login: username,
+      password: password
+    })
+      .then(response => {
+        if (response.status === 200) {
+          this.route.navigate(['/']);
+        }
+
+      })
+      .catch(error => {
+        if (error.response.status === 404) {
+          console.error('Пользователь не найден');
+        } else {
+          console.error('Ошибка при отправке запроса:', error.response.data);
+        }
+      });
 
   }
 }

2f430719b37d5bc5f269e761969a470d77164403
kolba <kolbasa20022002@mail.ru>
2024-02-19 16:57:19 +0300

    add data-picker filter

--- src/app/rent-car-dialog/rent-car-dialog.component.ts
+++ src/app/rent-car-dialog/rent-car-dialog.component.ts
@@ -8,6 +8,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {JsonPipe, NgIf, NgOptimizedImage} from "@angular/common";
 import {CarService} from "../car.service";
+import axios from "axios";
 
 
 @Component({
@@ -43,8 +44,6 @@ export class RentCarDialogComponent implements OnInit{
   totalPrice: any;
   numberOfDays: any;
 
-
-
   constructor(public dialogRef: MatDialogRef<RentCarDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any, private carService: CarService) {
     this.carId = data.carId;
@@ -60,17 +59,39 @@ export class RentCarDialogComponent implements OnInit{
       this.startDate = value.start;
       this.endDate = value.end;
       if (this.startDate && this.endDate) {
-        this.numberOfDays = Math.floor((this.endDate.getTime() - this.startDate.getTime()) / (1000 * 60 * 60 * 24));
-        this.totalPrice = Math.round(this.numberOfDays * (this.selectedCar?.PriceCarDay ?? 0));
+        const startDate =  this.startDate.getDate()+ '.' + (this.startDate.getMonth() + 1) + '.' + this.startDate.getFullYear()
+        const endDate =  this.endDate.getDate()+ '.' + (this.endDate.getMonth() + 1) + '.' + this.endDate.getFullYear()
+        this.getNumberOfDays(startDate, endDate);
       }
     });
   }
+  private getNumberOfDays(startDate: string, endDate: string) {
+    axios.post('http://localhost:5155/rentCar', {
+      CarId: this.carId,
+      startDateRent: startDate,
+      endDateRent: endDate
+    })
+      .then(response => {
+        if (response.status === 200) {
+          this.numberOfDays = response.data.numberOfDays
+          this.totalPrice = response.data.totalPrice
+        }
 
+      })
+
+  }
   cancel() {
     this.dialogRef.close();
   }
 
   confirm() {
     this.dialogRef.close();
+  }
+
+  filterPastDates(date: Date): boolean {
+    const now = new Date();
+    const oneYearFromNow = new Date();
+    oneYearFromNow.setFullYear(now.getFullYear() + 1);
+    return date >= now && date <= oneYearFromNow;
   }
 }
